fa31ab1d255baa68abf23c6aadeb3499cfc46709
licod1997 <licod1997@gmail.com>
2017-07-25 16:50:01 +0900

    upload project

---
+++ src/chatapplication/OutputThread.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chatapplication;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import javax.swing.JOptionPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Notebook
+ */
+public class OutputThread extends Thread {
+
+    Socket socket;
+    JTextArea txt;
+    BufferedReader bf;
+    String sender;
+    String receiver;
+
+    public OutputThread(Socket s, JTextArea txt, String sender, String receiver) {
+        super();
+        this.socket = s;
+        this.txt = txt;
+        this.sender = sender;
+        this.receiver = receiver;
+        try {
+            bf = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Network Error!");
+            System.exit(0);
+        }
+    }
+
+    public void run() {
+        while (true) {
+            try {
+                if (socket != null) {
+                    String msg = "";
+                    if ((msg = bf.readLine()) != null && msg.length() > 0) {
+                        char x = (char) 6;
+                        String y = Character.toString(x);
+                        if (msg.contains(y)) {
+                            String newMsg = msg.replace(x, '\n');
+                            txt.append("\n" + receiver + ": " + newMsg);
+                        } else {
+                            txt.append("\n" + receiver + ": " + msg);
+                        }
+                    }
+                }
+                sleep(1000);
+            } catch (Exception e) {
+            }
+        }
+    }
+}
